6769cdd9bfde6609b7499db8da7dd333face858d
AlanSquid <squid78313@gmail.com>
2022-10-04 23:57:56 +0800

    render index

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express()
 const mongoose = require('mongoose')
 const port = 3000
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json').results
+// const restaurantList = require('./restaurant.json').results
+const Restaurant = require('./models/restaurant.js')
 
 mongoose.connect(process.env.MONGODB_RESTAURANT_URI)
 
@@ -25,7 +26,10 @@ app.use(express.static('public'))
 
 // routes setting
 app.get('/', (req, res) => {
-  res.render('index', { restaurantList })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
 })
 
 app.get('/restaurants/:id', (req, res) => {
