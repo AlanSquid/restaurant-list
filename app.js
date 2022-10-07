3b769fea9f203ab1c99d186e8e0d289c8e6b56b7
AlanSquid <squid78313@gmail.com>
2022-10-07 15:23:23 +0800

    refactor: modify mongoose config'

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -8,17 +7,7 @@ const port = 3000
 const routes = require('./routes')
 const app = express()
 
-
-mongoose.connect(process.env.MONGODB_RESTAURANT_URI)
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 
 // setting temlate engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,16 +1,8 @@
-const mongoose = require('mongoose')
 const Restaurant = require('../restaurant.js')
+const db = require('../../config/mongoose')
 const restaurantList = require('../../restaurant.json').results
 
-mongoose.connect(process.env.MONGODB_RESTAURANT_URI)
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
 db.once('open', () => {
-  console.log('mongodb connected!')
   Restaurant.create(restaurantList)
   console.log('done')
 })
