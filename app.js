fb5aa62384c1e1182f96a2250513ea6536c0927b
AlanSquid <squid78313@gmail.com>
2022-10-23 23:33:23 +0800

    add user login feature via passport

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const usePassport = require('./config/passport')
 const port = 3000
 
 const routes = require('./routes')
@@ -23,6 +24,8 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+
+usePassport(app)
 
 app.use(routes)
 
