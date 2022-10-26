6c3fe73c118cee5cc9158e4859877a1777a46861
AlanSquid <squid78313@gmail.com>
2022-10-26 21:47:13 +0800

    add facebook login & dotenv

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const methodOverride = require('method-override')
 const session = require('express-session')
 const usePassport = require('./config/passport')
 const flash = require('connect-flash')
-const port = 3000
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const routes = require('./routes')
 const app = express()
+const PORT = process.env.PORT
 
 require('./config/mongoose')
 
@@ -48,6 +52,6 @@ app.use((req, res, next) => {
 app.use(routes)
 
 // start and listen on the Express server
-app.listen(port, () => {
-  console.log(`Express is listening on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Express is listening on http://localhost:${PORT}`)
 })
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,14 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 
 const { authenticator } = require('../middleware/auth')
 
 // 要驗證才能使用的，需加入middleware設定好的authenticator
 // 注意排放順序，不嚴謹的、不用驗證的放最上面
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/restaurants', authenticator, restaurants)
 router.use('/', authenticator, home)
 
