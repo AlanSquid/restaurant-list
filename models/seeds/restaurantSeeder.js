60eb9558de8b4481b45624accb2dac861bca5dfa
AlanSquid <squid78313@gmail.com>
2022-11-02 13:33:27 +0800

    modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -6,47 +6,35 @@ if (process.env.NODE_ENV !== 'production') {
 const Restaurant = require('../restaurant')
 const User = require('../user')
 const db = require('../../config/mongoose')
-const { Promise } = require('mongoose')
-const restaurantList = require('../../restaurant.json').results
-
-const SEED_USER1 = {
-  name: 'user1',
-  email: 'user1@example.com',
-  password: '12345678'
-}
-
-const SEED_USER2 = {
-  name: 'user2',
-  email: 'user2@example.com',
-  password: '12345678'
-}
-
-const seedUsers = [SEED_USER1, SEED_USER2]
-
+const restaurantList = require('./restaurant').results
+const userList = require('./user').results
 
 
 db.once('open', () => {
-  for (let i = 0; i < 2; i++) {
-    bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(seedUsers[i].password, salt))
-      .then(hash => User.create({
-        name: seedUsers[i].name,
-        email: seedUsers[i].email,
-        password: hash
-      }))
-      .then(user => {
-        const userId = user._id
-        for (let j = (i * 3); j < ((i + 1) * 3); j++) {
-          restaurantList[j].userId = userId
-        }
-        return Promise.all([Restaurant.create(restaurantList.slice(i * 3, ((i + 1) * 3)))])
+  Promise.all(
+    userList.map((user, userInidex) => {
+      return User.create({
+        name: user.name,
+        email: user.email,
+        password: bcrypt.hashSync(user.password, 5)
       })
-      .then(() => {
-        console.log('done!')
-        process.exit()
-      })
-  }
+        .then(user => {
+          const userRestaurants = []
+          restaurantList.forEach((restaurant, restIndex) => {
+            if (restIndex >= 3 * userInidex && restIndex < 3 * (userInidex + 1)) {
+              restaurant.userId = user._id
+              userRestaurants.push(restaurant)
+            }
+          })
+          return Restaurant.create(userRestaurants)
+        })
+    })
+  )
+    .then(() => {
+      console.log('done!')
+      process.exit()
+    })
+    .catch(err => console.log(err))
 })
 
 
